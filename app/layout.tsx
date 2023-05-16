8d05687ecbb2b54195cf205193d7db4c4a8a1a7f
dai <1lanyeue@gmail.com>
2023-05-16 21:25:12 +0700

    firt commit

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import './globals.css'
+import AuthContext from './context/AuthContext'
+import ActiveStatus from './components/ActiveStatus'
+import ToasterContext from './context/ToasterContext'
+import React from "react";
+
+export const metadata = {
+  title: 'Messenger',
+  description: 'Messenger',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <AuthContext>
+          <ToasterContext />
+          <ActiveStatus />
+          {children}
+        </AuthContext>
+      </body>
+    </html>
+  )
+}
